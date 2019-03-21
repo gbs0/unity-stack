eab4bb86d9bff9484f1bc8e2a6174aba931c51ee
gabriel schiavo <gabriel@lewagon.org>
2019-03-21 17:59:50 -0300

    add for loop

--- back-end/iterators.cs
+++ back-end/iterators.cs
@@ -1,19 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 public class UIScript : MonoBehaviour
 {
-		public Text lifes;
-		public Text time;
-
-    public void ChangeLife(int value)
-    {
-    	lifes.text = "Lifes: " + value;
-    }
-
     void Start()
     {
     	// For loop
