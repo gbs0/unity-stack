3f311a3fa7292753cdd3b7b6526c8eb297e6841e
gabriel schiavo <gabriel@lewagon.org>
2019-03-26 19:29:32 -0300

    add axis variables

--- gameplay/cubeSpawner/cube.cs
+++ gameplay/cubeSpawner/cube.cs
@@ -6,5 +6,11 @@ public class Cube : MonoBehaviour {
 	public float upForce = 1f;
 	public float sideForce = .1f;
 
+	void Start() 
+	{
+		float xForce = Random.Range(-)
+		float yForce =
+		float zForce = 
+	}
 
 }
