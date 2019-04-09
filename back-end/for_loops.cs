40a2e5f05314d7b42ee707c7f35a5e45f0d83c2a
gabriel schiavo <gabriel@lewagon.org>
2019-04-09 19:05:26 -0300

    add for loop

--- back-end/for_loops.cs
+++ back-end/for_loops.cs
@@ -1,17 +1,18 @@
 public class Block : MonoBehaviour {
 	void blockCreator()
-    {
-    	int levelLines = Mathf.Clamp(level, 1, 6);
-    	for(int column = 0; column < levelLines; column++) {
-    		for (int row = 0; row < 5; row++) {
-	    		GameObject copia;
-	    		if (1 < level/2) copia = Instantiate(superblock, transform);
-	    		else copia = Instantiate(block, transform);
-	    		Vector2 newPosition = genesisBlock;
-	    		newPosition.x += 1.5f * row;
-	    		newPosition.y -= 0.8f * column;
-	    		copia.transform.position = newPosition;
-	    	}	
-    	}
-    }
+  {
+  	int levelLines = Mathf.Clamp(level, 1, 6);
+  	for(int column = 0; column < levelLines; column++) {
+  		for (int row = 0; row < 5; row++) {
+    		GameObject copia;
+    		if (1 < level/2) {
+    			copia = Instantiate(superblock, transform);
+    		} else copia = Instantiate(block, transform);
+    		Vector2 newPosition = genesisBlock;
+    		newPosition.x += 1.5f * row;
+    		newPosition.y -= 0.8f * column;
+    		copia.transform.position = newPosition;
+    	}	
+  	}
+  }
 }
