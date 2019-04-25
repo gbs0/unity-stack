1acd9d4bbb2fa65a23dcacf12744e340f1803db6
gabriel schiavo <gabriel@lewagon.org>
2019-04-25 13:02:04 -0300

    add missile script instantiate

--- gameplay/InstantiateMissile/Missile.cs
+++ gameplay/InstantiateMissile/Missile.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Example : MonoBehaviour
+public class Missile : MonoBehaviour
 {
     // Instantiate a rigidbody then set the velocity
 
@@ -19,6 +19,39 @@ public class Example : MonoBehaviour
             // Give the cloned object an initial velocity along the current
             // object's Z axis
             clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+    }
+}
+
+// Instantiate can also clone script instances directly. 
+// The entire game object hierarchy will be cloned and the cloned script instance will be returned.
+
+using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour
+{
+    public int timeoutDestructor;
+
+    // ...other code...
+}
+
+
+public class ExampleClass : MonoBehaviour
+{
+    // Instantiate a Prefab with an attached Missile script
+    public Missile projectile;
+
+    void Update()
+    {
+        // Ctrl was pressed, launch a projectile
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Missile clone = (Missile)Instantiate(projectile, transform.position, transform.rotation);
+
+            // Set the missiles timeout destructor to 5
+            clone.timeoutDestructor = 5;
         }
     }
 }
