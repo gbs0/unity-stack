40566a87b0587cff493e95f572a49ab4ea212454
gabriel schiavo <gabriel@lewagon.org>
2019-04-25 10:10:25 -0300

    add unity reference

--- back-end/getComponent_UI.cs
+++ back-end/getComponent_UI.cs
@@ -1,25 +1,28 @@
-  public class EditText()
-   {
-              Text myText;
-         
-         public void blablabla()  
-         // not that your function must be public if you want to atach the script to your text
-         {
-                  myText=GameObject.Find("MainText").GetComponent<Text>(); 
-                  // here the variable myText reference to the game Object MainText
-           
-                  myText.text="Hello";    
-           
-                  // or you can also do this
-                  string example="Hello2";
-                  myText.text=example // this will show you Hello2
-         
-                 // there are also other function for example to change color
-         
-                 myText.color=Color.red // your text color will  be red
-          
-         
-         
-             } // end of the funtion
-         
-  } // end of the class
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditText()
+{
+  Text myText;
+     
+  public void blablabla()  
+  // not that your function must be public if you want to atach the script to your text
+  {
+    myText=GameObject.Find("MainText").GetComponent<Text>(); 
+    // here the variable myText reference to the game Object MainText
+
+    myText.text="Hello";    
+
+    // or you can also do this
+    string example="Hello2";
+    myText.text=example // this will show you Hello2
+
+   // there are also other function for example to change color
+
+   myText.color=Color.red // your text color will  be red
+
+
+
+  }   
+} 
