d5ffb19118fc75f1f0106ea112fe0c07f62eed73
gabriel schiavo <gabriel@lewagon.org>
2019-04-25 13:00:28 -0300

    add missile script

---
+++ gameplay/InstantiateMissile/Missile.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour
+{
+    // Instantiate a rigidbody then set the velocity
+
+    Rigidbody projectile;
+
+    void Update()
+    {
+        // Ctrl was pressed, launch a projectile
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+
+            // Give the cloned object an initial velocity along the current
+            // object's Z axis
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+    }
+}
