c2774c900f7cbb9d9aa03d826aab382a3f1312d7
gabriel schiavo <gabriel@lewagon.org>
2019-04-25 10:18:06 -0300

    refactoring

--- back-end/getComponent_UI.cs
+++ back-end/getComponent_UI.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EditText()
 {
+  
   Text myText;
      
-  public void blablabla()  
-  // not that your function must be public if you want to atach the script to your text
-  {
+  void Start() {
+    // Here the variable myText reference to the game Object MainText
     myText=GameObject.Find("MainText").GetComponent<Text>(); 
-    // here the variable myText reference to the game Object MainText
+    
+  }
 
-    myText.text="Hello";    
+  // Note that your function must be public if you want to atach the script to your text
+  public void blablabla()  
+  {
+    myText.text = "Hello";    
 
     // or you can also do this
     string example="Hello2";
@@ -21,8 +25,5 @@ public class EditText()
    // there are also other function for example to change color
 
    myText.color=Color.red // your text color will  be red
-
-
-
   }   
 } 
