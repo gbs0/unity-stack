87dd2d51a1ce04a2dc46ff65a96e7a3c2e5740f1
gabriel schiavo <gabriel@lewagon.org>
2019-08-17 10:14:00 -0300

    generate function for add new cards to Deck

--- gameplay/cardGame/MainGame.cs
+++ gameplay/cardGame/MainGame.cs
@@ -7,5 +7,25 @@ public class MainGame : MonoBehavior
 	// Init String Array for card suits
 	public static string[] naipes = new string[] { "C", "D", "H", "S" };
 	// Init String Array for card values
-	public static string[] values = new string[] { "A","1","2","3", "4", "5", "6", "7", "8","9", "K", "Q", "J" };
+	public static string[] values = new string[] { "A","1","2","3", "4", "5", "6", "7", "8","9","10", "K", "Q", "J" };
+
+	void Start(){
+
+	}
+
+	void Update(){
+
+	}
+
+	public static List<string> GerarDeck()
+	{
+		List<string> deckNovo = new List<string>();
+		
+		foreach (string naipe in naipes) {
+			foreach (string value in values) {
+				deckNovo.Add(naipe + value);
+			}
+		}
+	}
+
 }
