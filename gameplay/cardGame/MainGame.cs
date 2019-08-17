4b90a7d9f8dddad2c0d4c3ebd8d4039b3a802a79
gabriel schiavo <gabriel@lewagon.org>
2019-08-17 10:07:15 -0300

    add script for hold card infos

---
+++ gameplay/cardGame/MainGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehavior
+{
+	// Init String Array for card suits
+	public static string[] naipes = new string[] { "C", "D", "H", "S" };
+	// Init String Array for card values
+	public static string[] values = new string[] { "A","1","2","3", "4", "5", "6", "7", "8","9", "K", "Q", "J" };
+}
