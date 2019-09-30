9c9f36b379f56705d4074492dbeee3cc9e4da977
gabriel schiavo <gabriel@lewagon.org>
2019-09-30 15:20:22 -0300

    automatted commit

--- gameplay/randomItem/RandomItemGenerator.cs
+++ gameplay/randomItem/RandomItemGenerator.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 public class RandomItemGenerator : MonoBehavior
 {
 	string[] itemName = new string[] {
+		
 
 	};
 
 	string[] itemDescription = new string[]{	
-
+		"finished World of Warcraft",
+		"knows the last digit of PI",
+		"is immutable. If something's going to change, it's going to have to be the rest of the universe!",
+		"doesn't need an OS",
+		"can instantiate an abstract class",
+		"rewrote the Google search engine from scratch",
+		"doesn't have disk latency because the hard drive knows to hurry the hell up",
+		"doesn't need to know about class factory pattern. He can instantiate interfaces",
+		"can recite π. Backwards!",
+		"can binary search unsorted data",
+		"doesn't need a debugger, he just stares down the bug until the code confesses",
+		"keyboard doesn't have a F1 key, the computer asks him for help"
 	};
 
 	string[] itemDimension = new string[] {
