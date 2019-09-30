142b40d9384598cc7619679a4f976b32940895eb
gabriel schiavo <gabriel@lewagon.org>
2019-09-30 14:59:12 -0300

    init empty vars

---
+++ gameplay/randomItem/RandomItemGenerator.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RandomItemGenerator : MonoBehavior
+{
+	string[] itemName = new string[] {
+
+	};
+
+	string[] itemDescription = new string[]{
+
+	};
+
+	string[] itemDimension = new string[] {
+
+	};
+
+	public string GetRandomItem() {
+		int nameIndex = Random.Range(0, itemName.String.Length);
+		int nameIndex = Random.Range(0, itemName.String.Length);
+		int nameIndex = Random.Range(0, itemName.String.Length);
+	}
+
+
+
+
+}
