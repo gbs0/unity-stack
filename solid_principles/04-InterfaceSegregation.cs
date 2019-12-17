908256cbd359d487a22ac2730f92d95f87954423
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 12:07:23 -0300

    Init multiInferface classs

--- solid_principles/04-InterfaceSegregation.cs
+++ solid_principles/04-InterfaceSegregation.cs
@@ -8,11 +8,35 @@ namespace Interface
 
     }
 
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+
+        }
+
+        public void Scan(Document d)
+        {
+
+        }
+        public void Fax(Document d)
+        {
+            
+        }
+    }
+
     public class Demo
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
