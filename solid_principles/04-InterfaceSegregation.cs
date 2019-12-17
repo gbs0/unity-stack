936b753558a08d9c6d16497aea891c8220f0749a
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 11:56:25 -0300

    add first class for fourth step

---
+++ solid_principles/04-InterfaceSegregation.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Interface
+{
+    public class Document
+    {
+
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
