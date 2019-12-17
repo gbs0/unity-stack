249b237ad0483921345a65a53f3bb4097d139b1d
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 12:35:42 -0300

    add decorators and splitted interfaces

--- solid_principles/04-InterfaceSegregation.cs
+++ solid_principles/04-InterfaceSegregation.cs
@@ -74,9 +74,36 @@ namespace Interface
 
     public interface IMultiFunctionDevice : IScanner, IPrinter //..
     {
-        
+
     }
     
+    public class MultiFunctionMachine : IMultiFunctionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        } // Decorator Pattern
+    }
 
 
     public class Demo
