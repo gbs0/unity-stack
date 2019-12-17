78459a9e40467afa05b1b8ac1c6506aa1411a90d
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 11:26:53 -0300

    add gets and sets for variable values

--- solid_principles/03-LiskovSubstitution.cs
+++ solid_principles/03-LiskovSubstitution.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 
 namespace DesignPatterns
 {
-    
+     public class Rectangle // Init basic Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int)
+    }
     public class Liskov
     {
-        
+        static void Main(string[] args)
     }
 }
