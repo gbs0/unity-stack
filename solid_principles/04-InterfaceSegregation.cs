296badd4896f2a9d236b44c4debe670f405ef577
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 12:11:02 -0300

    throw new Excpetions errors

--- solid_principles/04-InterfaceSegregation.cs
+++ solid_principles/04-InterfaceSegregation.cs
@@ -26,9 +26,25 @@ namespace Interface
         {
 
         }
+
         public void Fax(Document d)
         {
-            
+
+        }
+    }
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+
+        }
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
         }
     }
 
