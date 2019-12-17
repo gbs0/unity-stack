70edf8f8796ac03702d6505dd025fe6cdac932f1
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 13:06:17 -0300

    comment code for better syntax

--- solid_principles/05-DependencyInversion.cs
+++ solid_principles/05-DependencyInversion.cs
@@ -17,6 +17,11 @@ namespace DesignPatterns
         // public DateTime DateOfBirth;
     }
 
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
     // low-level parts of the system
     public class Relationships
     {
@@ -29,22 +34,22 @@ namespace DesignPatterns
             relations.Add((child, Relationship.Parent, parent));
         }
 
-        public List<(Person, Relationship, Person)> Relations => relations;
+        // public List<(Person, Relationship, Person)> Relations => relations;
     }
 
     public class Search
     {
-        public Search(Relationships relationships)
-        {
-            var relations = relationships.Relations;
-            foreach (var r in relations.Where(
-                x => x.Item1.Name == "John" &&
-                     x.Item2 == Relationship.Parent
-            ))
-            {
-                WriteLine($"John has a child called {r.Item3.Name}");
-            }
-        }
+        // public Search(Relationships relationships)
+        // {
+        //     var relations = relationships.Relations;
+        //     foreach (var r in relations.Where(
+        //         x => x.Item1.Name == "John" &&
+        //              x.Item2 == Relationship.Parent
+        //     ))
+        //     {
+        //         WriteLine($"John has a child called {r.Item3.Name}");
+        //     }
+        // }
 
         static void Main(string[] args)
         {
