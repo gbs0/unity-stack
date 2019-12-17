1b3aed543e20ed1fef62b9f6e8fb04b138719cf7
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 11:50:31 -0300

    add public override for Liskov Princciple

--- solid_principles/03-LiskovSubstitution.cs
+++ solid_principles/03-LiskovSubstitution.cs
@@ -5,8 +5,8 @@ namespace DesignPatterns
 {
      public class Rectangle // Init basic Rectangle
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
 
         public Rectangle()
         {
@@ -22,20 +22,28 @@ namespace DesignPatterns
         public override string ToString()
         {
             return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }        
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
         }
 
-        public CalculateArea() 
+        public override int Height
         {
-
+            set { base.Width = base.Height = value; }
         }
     }
-    public class Liskov
+    public class LiskovSubstitution
     {
         static public int Area(Rectangle r) => r.Width * r.Height;
         static void Main(string[] args)
         {
             var rect = new Rectangle();
-            WriteLine($"{rect} has area {Area(rect)}")
+            WriteLine($"{rect} has area {Area(rect)}");
 
         }
     }
