77c91326ed1e4310b416f3bdf103416a465b238a
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 10:53:29 -0300

    add some size specification use OpenClosed principle

--- solid_principles/OpenClosedPrinciple.cs
+++ solid_principles/OpenClosedPrinciple.cs
@@ -75,6 +75,18 @@ namespace DesignPatterns
     }
 
     public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
 
     public class BetterFilter : IFilter<Product>
     {
