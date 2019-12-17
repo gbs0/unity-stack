ed3710a49355f8f5876cdc781c90016368f976ae
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-17 12:44:03 -0300

    add relashionship relation

--- solid_principles/05-DependencyInversion.cs
+++ solid_principles/05-DependencyInversion.cs
@@ -3,5 +3,29 @@ using System.Collections.Generic;
 
 namespace DesignPatterns
 {
-    
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+        // public string Lastname;
+        // public DateTime DateOfBirth;
+    }
+
+    // low-level parts of the system
+    public class Relationships
+    {
+        private List<(Person, Relationship, Person)> relations 
+        = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            
+        }
+    }
 }
