c3e7eaabfe7bbfd7e0069f7febac9b958349682c
gabriel schiavo <gabriel@lewagon.org>
2020-02-17 22:17:03 -0300

    add script for Item instace

---
+++ gameplay/inventorySystem/Item.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Item", menuName ="Item/baseItem")]
+
+public class Item : ScriptableObject
+{
+    new public string name = "Defulat Item";
+    public Sprite icon = null;
+
+    public virtual void Use()
+    {
+        Debug.Log("Using " + name);
+    }
+}
